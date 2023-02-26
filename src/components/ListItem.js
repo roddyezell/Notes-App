4a65dfecde34826c2562673ac836d55ddff3db2d
Roddy Ezell <ezellroddy@gmail.com>
2023-02-25 20:22:54 -0500

    Starter CSS, more routing

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function ListItem({note}) {
+export default function ListItem({ note }) {
+
+    
 
     return (
-        <div>
+        
+        <Link to={`/note/${note.id}`}>
+            
             <h3>{note.body}</h3>
-        </div>
+            
+        </Link>
     )
 }
 
--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useParams } from "react-router-dom"
+import notes from '../assets/data'
 
 export default function NotesListPage() {
+    
+
+    let { id } = useParams();
+    let note = notes.find(note => note.id === Number({id}.id))
+    
+    // console.log(`${id}`);
+
     return(
         <div>
-            <h1>This is a single note page</h1>
+            <p>{note?.body}</p>
         </div>
     )
 }
